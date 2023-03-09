0b73a28fa351d84942ee4182846e3dbedf66ae02
Aditya <91947505+adityapotdar23@users.noreply.github.com>
2023-03-09 21:45:31 +0530

    Create reverse_the_array.cpp

---
+++ reverse_the_array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+void reverseArray(vector<int> &arr , int m)
+{
+	// Write your code here. 
+	int n = arr.size(); 
+	int start = m + 1;
+	int end = n - 1;
+
+	while(start<end)
+	{
+		swap(arr[start], arr[end]);
+		start++; 
+		end--;
+	}
+	
+	
+}
