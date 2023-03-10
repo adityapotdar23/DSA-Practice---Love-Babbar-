01c737ffae2591eb4e2b0df04e871039a0e66443
Aditya <91947505+adityapotdar23@users.noreply.github.com>
2023-03-10 08:41:57 +0530

    Delete sum_of_max_and_min.cpp

--- sum_of_max_and_min.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h> 
-int sumOfMaxMin(int arr[], int n){
-	// Write your code here.
-	// int leng = sizeof(arr)/sizeof(arr[0]);
-	sort(arr, arr+n); 
-	return arr[0] + arr[n-1];
-}
